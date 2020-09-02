4c63dd25f7f0de96647dbc73bdb4052736f8b202
Max Howard <max@nanome.ai>
2020-09-02 13:30:06 -0700

    remove unnecessary line

--- docker/set_settings.sh
+++ docker/set_settings.sh
@@ -2,7 +2,6 @@
 
 docker cp ./settings.json nanome-postgnome:/root/Documents/nanome-plugins/postgnome/settings.json
 
-# docker exec nanome-postgnome chgrp --help
 docker exec nanome-postgnome chown root /root/Documents/nanome-plugins/postgnome/settings.json
 docker exec nanome-postgnome chgrp nogroup /root/Documents/nanome-plugins/postgnome/settings.json
 docker restart -t0 nanome-postgnome
